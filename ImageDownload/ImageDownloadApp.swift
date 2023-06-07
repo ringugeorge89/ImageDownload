e4aadcb9b0c45fe242812b799af381ac94771687
Ringu <ringugeorge@gmail.com>
2023-06-07 07:54:44 +0530

    Initial Commit

---
+++ ImageDownload/ImageDownloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDownloadApp.swift
+//  ImageDownload
+//
+//  Created by Mohan on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageDownloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
